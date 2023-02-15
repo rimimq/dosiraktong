5a66625777564ecab9873fce713113d84db1a5e8
rimimq <dbfla65486@naver.com>
2023-02-15 19:00:17 +0900

    2/15

--- js/script.js
+++ js/script.js
@@ -100,5 +100,87 @@ window.onload = function () {
         });
     }
 
-    new Swiper(".swvisual");
+    //data.joun 외부연동
+    //1. XMLHttpRequest 활용(반드시 JSON.parse()실행)
+    const xhttp = new XMLHttpRequest();
+    //data.json 이 불러들여졌는지 검사 후 완료 시 실행
+    xhttp.onreadystatechange = function (e) {
+        const req = e.target;
+        if (req.readyState === XMLHttpRequest.DONE) {
+            console.log(req.response);
+            //아래 구문을 반드시 체크.
+            const dataArr = data.visual;
+            console.log(dataArr);
+        }
+    }
+    // xhttp.open("GET", "data.json");
+    // xhttp.send();
+
+    //2. fetch 활용 (아래구문을 준수. JSON.parse를 할 필요가 없음.)
+    fetch("data.json")
+        .then((res) => {
+            res.json();
+        })
+        .then((data) => {
+            //데이터를 활용
+            // console.log(data.visual)
+            const dataArr = data.visual;
+            //데이터를 외부 변수에 저장
+            visualData = dataArr;
+            console.log(visualData);
+            showVT(visualData[0]);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    //비주얼에 활용할 데이터
+    let visualData;
+    //화면에 데이터를 출력하는 함수
+    const swvTitle = document.querySelector(".swvisual-title");
+    const swvTxt = document.querySelector(".swvisual-txt");
+    const swvLink = document.querySelector(".swvisual-link");
+    const swList = document.querySelectorAll(".swvisual-list li")
+    // 타이틀 내용 보여주기
+    function showVT(_data) {
+        swvTitle.innerHTML = _data.title;
+        swvTxt.innerHTML = _data.txt;
+        if (_data.link === "no") {
+            swvLink.classList.add("active");
+        } else {
+            swvLink.classList.remove("active");
+        }
+    }
+    //포커스 라인 애니메이션 실행함수
+    function changeBar(_idx) {
+        swList.forEach((item, index) -> {
+            if(index === _idx) {
+            item.classListadd("active");
+        }
+    });
+}
+
+
+// 비주얼 슬라이드
+let swvisual = new Swiper(".swvisual", {
+    effect: "fade",
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        prevEl: ".swvisual-prev",
+        nextEl: ".swvisual-next"
+    }
+});
+//슬라이드가 변경될 때마다 하고 싶은 일 진행
+swvisual.on("slideChange", function () {
+    console.log("진짜 html 태그의 순서", swvisual.realIndex);
+    console.log("모션이 되는 순서", swvisual.realIndex);
+    //텍스트를 수정
+    showVT(visualData[swvisual.realIndex]);
+});
+
 }
